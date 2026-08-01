b3374d05b47b7e89138fabeaf2862bcc7be3560f
Miguel Angel De la Vega Rodriguez <miguel.delavega@wazuh.com>
2026-08-01 09:26:53 +0200

    feat: allow to query std/ directory to print docs
    Just to get the idea, it should be refactored later and will be

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    // This idk, maybe extract to method
+    let mut arg = std::env::args();
+    if arg.len() != 2 {
+        eprintln!("Usage: $ cppdoc <std::crate::method>");
+        std::process::exit(1);
+    }
+
+    let method_name = arg.next().unwrap();
+    let doc_path = method_name.replace("::", "/");
+
+    // This has to be done for each path in std/
+    // in comp time
+    let mut map = HashMap::new();
+    let push_back = include_str!("../std/vector/push_back.md");
+    // So this path is not runtime-handled
+    map.insert(doc_path.clone(), push_back);
+
+    // Then this is done at runtime with path
+    println!("{}", map.get(&doc_path).unwrap_or(&"Not found"));
 }
